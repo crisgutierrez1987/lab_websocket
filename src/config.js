e2c3a7efc0a02c7819f20d807c3838291882ac3a
Cris Veronica Gutierrez Lopez <cris.gutierrez@sophossolutions.com>
2022-07-13 19:28:47 -0500

    :sparkles: implementing websocket client

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const URL_WS = 'wss://ws.bitstamp.net/';
+
+export const TITLE = 'Dólar & Bitcoin';
+
+export const AUTOR = 'Cris Verónica Gutiérrez';
+
+export const YEAR = new Date().getFullYear();
